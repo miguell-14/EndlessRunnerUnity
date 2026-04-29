2218ecea9a96e5423b54b6a0543e8244813f6796
miguell-14 <miguelrebouco@gmail.com>
2026-04-29 14:17:55 +0100

    fix: troca de carro

--- Assets/Scripts/AI/AICarSpawner.cs
+++ Assets/Scripts/AI/AICarSpawner.cs
@@ -7,19 +7,22 @@ public class AI : MonoBehaviour
     [SerializeField]
     GameObject[] carAIPreFabs;
 
-    GameObject[] carAIPool = new GameObject[20];
+    GameObject[] carAIPool = new GameObject[30];
 
     Transform playerCarTransform;
+
+    // NOVO: posições das lanes (podes editar no Inspector)
+    [SerializeField]
+    float[] lanePositions = new float[] { -2f, 0f, 2f };
 
     //Timing
     float timeLastCarSpawned = 0;
     WaitForSeconds wait = new WaitForSeconds(0.5f);
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         playerCarTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
-        
         int prefabIndex = 0;
 
         for (int i = 0; i < carAIPool.Length; i++)
@@ -29,10 +32,8 @@ public class AI : MonoBehaviour
 
             prefabIndex++;
 
-            //Loop the prefab index if we run out of prefabs
-            if (prefabIndex > carAIPreFabs.Length -1)
+            if (prefabIndex > carAIPreFabs.Length - 1)
                 prefabIndex = 0;
-            
         }
 
         StartCoroutine(UpdateLessOfTenCO());
@@ -44,22 +45,20 @@ public class AI : MonoBehaviour
         {
             CleanUpCarsBeyondView();
             SpawnNewCars();
-            
+
             yield return wait;
         }
     }
 
     void SpawnNewCars()
     {
-        if (Time.time - timeLastCarSpawned < 2)
+        if (Time.time - timeLastCarSpawned < 0.5f)
             return;
 
         GameObject carToSpawn = null;
 
-        //Find a car to spawn
         foreach (GameObject aiCar in carAIPool)
         {
-            //Skip active cars
             if (aiCar.activeInHierarchy)
                 continue;
 
@@ -67,11 +66,13 @@ public class AI : MonoBehaviour
             break;
         }
 
-        //No car avaiable to spawn
         if (carToSpawn == null)
             return;
 
-        Vector3 spawnPosition = new Vector3(0, 0, playerCarTransform.transform.position.z + 100);
+        // ESCOLHER LANE ALEATÓRIA
+        float randomX = lanePositions[Random.Range(0, lanePositions.Length)];
+
+        Vector3 spawnPosition = new Vector3(randomX, 0, playerCarTransform.position.z + 100);
 
         carToSpawn.transform.position = spawnPosition;
         carToSpawn.SetActive(true);
@@ -79,27 +80,23 @@ public class AI : MonoBehaviour
         timeLastCarSpawned = Time.time;
     }
 
-
     void CleanUpCarsBeyondView()
     {
         foreach (GameObject aiCar in carAIPool)
         {
-            //Skip inactive cars
             if (!aiCar.activeInHierarchy)
                 continue;
 
-            //Check if AI car is too far ahead
             if (aiCar.transform.position.z - playerCarTransform.position.z > 200)
                 aiCar.SetActive(false);
 
-            //Check if AI car is too far behind
-            if(aiCar.transform.position.z - playerCarTransform.position.z < -50)
+            if (aiCar.transform.position.z - playerCarTransform.position.z < -50)
                 aiCar.SetActive(false);
         }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -4,7 +4,12 @@ using System.Collections.Generic;
 
 public static class Utils
 {
-    static float[] carlanes = {-0.65f, 0.65f};
+    static float[] carlanes = 
+    { 
+        -0.65f, 
+         0f,       
+         0.65f   
+    };
+
     public static float[] CarLanes => carlanes;
-    
 }
